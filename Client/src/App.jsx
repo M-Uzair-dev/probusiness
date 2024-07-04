057bdb6dcb877c0dff0ad7b557174ea90eec4711
Uzair manan <uzairmanan3@gmail.com>
2024-07-04 11:16:54 +0500

    fixed bugs

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,21 @@
-import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Toaster } from "sonner";
 import "./index.css";
 
 const App = () => {
+  const navigate = useNavigate();
+  const loggedin = localStorage.getItem("userID");
   const [pass, setPass] = useState(false);
-  setTimeout(() => {
-    setPass(true);
-  }, 1000);
+  useEffect(() => {
+    if (loggedin) {
+      setPass(true);
+    } else {
+      navigate("/login");
+      setPass(true);
+    }
+  }, []);
+
   return (
     <div>
       <Toaster richColors position="bottom-left" closeButton />
