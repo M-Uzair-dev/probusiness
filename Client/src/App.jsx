a21ac5f42cfda50ec1e029a6da40d416f60d983d
Uzair manan <uzairmanan3@gmail.com>
2024-07-04 11:11:56 +0500

    aded loading bar

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
 import "./index.css";
 
 const App = () => {
+  const [pass, setPass] = useState(false);
+  setTimeout(() => {
+    setPass(true);
+  }, 1000);
   return (
     <div>
       <Toaster richColors position="bottom-left" closeButton />
-      <Outlet />
+      {pass ? <Outlet /> : <h3>Loading...</h3>}
     </div>
   );
 };
